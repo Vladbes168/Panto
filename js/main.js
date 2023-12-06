361cf1e6e2fe7b07992157d5e96ca3e9fd76f05a
Vlad <vd1153132@gmail.com>
2023-12-06 20:14:26 +0100

    make slider

--- js/main.js
+++ js/main.js
@@ -17,15 +17,48 @@ function showHint(e) {
 document.addEventListener('click', closeHints);
 
 function closeHints() {
-for (let hint of infoHints) {
-    hint.classList.add('none');
-}
+    for (let hint of infoHints) {
+        hint.classList.add('none');
+    }
 }
 
 //Запрет 
 
 for (let hint of infoHints) {
-    hint.addEventListener('click', (e) => e.stopPropagation() );
+    hint.addEventListener('click', (e) => e.stopPropagation());
 }
 
 
+// Swiper slider
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+    freeMode: true,
+    slidesPerView: 4,
+    spaceBetween: 42,
+    // breakpoints
+    // breakpoints: {
+    //     640: {
+    //         slidesPerView: 2,
+    //         spaceBetween: 20,
+    //     },
+    //     768: {
+    //         slidesPerView: 4,
+    //         spaceBetween: 40,
+    //     },
+    //     1024: {
+    //         slidesPerView: 5,
+    //         spaceBetween: 50,
+    //     },
+    // },
+
+    // Navigation arrows
+
+    navigation: {
+        nextEl: '#sliderNext',
+        prevEl: '#sliderPrev',
+    },
+
+});
